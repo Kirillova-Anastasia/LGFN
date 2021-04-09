4f99410936c92d89a0aec9918284fdbbfea83c0f
ansty <kirillovaap@yandex.ru>
2021-04-09 23:35:11 +0300

    script

--- LGFN.sh
+++ LGFN.sh
@@ -1,6 +1,4 @@
-docker run -it -v ~/mnt/calypso/24a_kir/MSU_VSR_Benchmark/input:/dataset:ro -v ~/mnt/calypso/24a_kir/MUS_VSR_Benchmark/result_on_noise2 --gpus '"device=2"' pytorch/pytorch:1.0.1-cuda10.0-cudnn7-devel
 pip install tensorboard==1.11.0 tensorboardX==1.4
-git clone https://github.com/Kirillova-Anastasia/LGFN.git
 cd LGFN/src/Deform_Conv
 sh make.sh
 cd ..
