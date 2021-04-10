279de557cf9b142c51d71ff6653eb93f86649654
ansty <kirillovaap@yandex.ru>
2021-04-10 09:58:55 +0300

    script

--- LGFN.sh
+++ LGFN.sh
@@ -3,4 +3,4 @@ cd src/Deform_Conv
 sh make.sh
 cd ..
 pip install matplotlib imageio scikit-image
-python3 main.py --test_only --save_results --pre_train /workspace/LGFN/experiment/LGFN/LGFN_x4.pt --dir_demo /dataset/test1_bicubic_noise2 --dir_demo_GT /dataset/test1_bicubic_noise2 --save_dir /output/LGFN/test1_bicubic_noise2
+python3 main.py --test_only --save_results --pre_train /workspace/scripts/LGFN/experiment/LGFN/LGFN_x4.pt --dir_demo /dataset/test1_bicubic_noise2 --dir_demo_GT /dataset/test1_bicubic_noise2 --save_dir /output/LGFN/test1_bicubic_noise2
