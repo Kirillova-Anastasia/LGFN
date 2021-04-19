c66da78094ec5026dcd82462430ba5d53a0151c2
ansty <kirillovaap@yandex.ru>
2021-04-19 23:54:26 +0300

    script

--- LGFN.sh
+++ LGFN.sh
@@ -3,13 +3,3 @@ cd src/Deform_Conv
 sh make.sh
 cd ..
 pip install matplotlib imageio scikit-image
-python3 main.py --test_only --save_results --pre_train /workspace/LGFN/experiment/LGFN/LGFN_x4.pt --dir_demo /dataset --video_name test1_bicubic_noise2 --save_dir /output
-python3 main.py --test_only --save_results --pre_train /workspace/LGFN/experiment/LGFN/LGFN_x4.pt --dir_demo /dataset --video_name test1_gauss_noise2 --save_dir /output
-
-pip install matplotlib imageio scikit-image
-python3 main.py --test_only --save_results --pre_train /workspace/LGFN/experiment/LGFN/LGFN_x4.pt --dir_demo /dataset --video_name test2_bicubic_noise2 --save_dir /output
-python3 main.py --test_only --save_results --pre_train /workspace/LGFN/experiment/LGFN/LGFN_x4.pt --dir_demo /dataset --video_name test2_gauss_noise2 --save_dir /output
-
-pip install matplotlib imageio scikit-image
-python3 main.py --test_only --save_results --pre_train /workspace/LGFN/experiment/LGFN/LGFN_x4.pt --dir_demo /dataset --video_name test3_bicubic_noise2 --save_dir /output
-python3 main.py --test_only --save_results --pre_train /workspace/LGFN/experiment/LGFN/LGFN_x4.pt --dir_demo /dataset --video_name test3_gauss_noise2 --save_dir /output
